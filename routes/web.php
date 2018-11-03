719aad53a5467836d19431e744c64c5e5e493331
yixvan6 <yixvan6@163.com>
2018-11-03 03:30:16 +0000

    Generated topics

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
