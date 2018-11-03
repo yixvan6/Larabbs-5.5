00eca2d31eb0cdb174fbeb5a8946742710e914c9
yixvan6 <yixvan6@163.com>
2018-11-03 07:05:49 +0000

    Category topic list

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,7 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
+
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
