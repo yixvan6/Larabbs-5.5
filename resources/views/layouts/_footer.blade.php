3db083314054222891ab4235053da6f6b2869b2f
yixvan6 <yixvan6@163.com>
2018-11-07 10:29:10 +0000

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="http://weibo.com/u/2824905012?is_hot=1" target="_blank">Hehorange</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('title', 'welcome') - LaraBBS4</title>
-    <meta name="description" content="@yield('description')">
+    <title>@yield('title', 'welcome') - {{ setting('site_name', 'LaraBBS4') }}</title>
+    <meta name="description" content="@yield('description', setting('seo_description'))">
+    <meta name="keyword" content="{{ setting('seo_keyword') }}">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
