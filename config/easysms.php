7c620efe333b611788483690d44956349a6b2971
yixvan6 <yixvan6@163.com>
2018-11-30 07:38:41 +0000

    add easysms

---
+++ config/easysms.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'timeout' => 5.0,
+
+    'default' => [
+        // 网关调用策略，默认为顺序调用
+        'strategy' => \Overtrue\EasySms\Strategies\OrderStrategy::class,
+
+        // 默认可用的发送网关
+        'gateways' => [
+            'yunpian',
+        ],
+    ],
+
+    // 可用的网关配置
+    'gateways' => [
+        'errorlog' => [
+            'file' => '/tmp/easy-sms.log',
+        ],
+        'yunpian' => [
+            'api_key' => env('YUNPIAN_API_KEY'),
+        ],
+    ],
+];
