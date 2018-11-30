7128f6f1287d306328d43bd2a6fe6c3fb8189d84
yixvan6 <yixvan6@163.com>
2018-11-30 08:29:12 +0000

    verificationCode store

---
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Overtrue\EasySms\EasySms;
+use App\Http\Requests\Api\VerificationCodeRequest;
+
+class VerificationCodesController extends Controller
+{
+    public function store(VerificationCodeRequest $request, EasySms $easysms)
+    {
+        $phone = $request->phone;
+
+        // 开发环境下可用假的 code
+        if ( ! app()->environment('production')) {
+            $code = '1234';
+        } else {
+            // 生成 4 位随机数作为验证码
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
+            // 发送
+            try {
+                $result = $easysms->send($phone, [
+                    'content' => "【呵呵橙】您的验证码是{$code}。如非本人操作，请忽略本短信"
+                ]);
+            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $e) {
+                $message = $e->getException('yunpian')->getMessage();
+                return $this->response->errorInternal($message ?? '短信发送异常');
+            }
+        }
+
+
+        // 缓存验证码，15 分钟后过期
+        $key = 'verificationCode_' . str_random(15);
+        $expiredAt = now()->addMinutes(15);
+
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return $this->response->array([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
+    }
+}
---
+++ database/migrations/2018_11_30_153914_add_phone_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPhoneToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email')->nullable()->change();
+            $table->string('phone')->nullable()->unique()->after('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropCloumn('phone');
+            $table->string('email')->nullable(false)->change();
+        });
+    }
+}
