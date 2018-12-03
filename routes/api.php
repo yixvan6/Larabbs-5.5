d8524de4836dd962b2d84098b16ac4a64684c108
yixvan6 <yixvan6@163.com>
2018-12-03 08:41:55 +0000

    notifications read

--- routes/api.php
+++ routes/api.php
@@ -95,6 +95,9 @@ $api->version('v1', [
             // 未读通知统计
             $api->get('user/notifications/stats', 'NotificationsController@stats')
                 ->name('api.user.notifications.stats');
+            // 标记消息为已读
+            $api->patch('user/read/notifications', 'NotificationsController@read')
+                ->name('api.user.notifications.read');
         });
     });
 });
