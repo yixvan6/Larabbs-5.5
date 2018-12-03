93c6241bb4a43e1095dd337b538606495bcf5063
yixvan6 <yixvan6@163.com>
2018-12-03 08:32:53 +0000

    notifications stats

--- routes/api.php
+++ routes/api.php
@@ -92,6 +92,9 @@ $api->version('v1', [
             // 通知列表
             $api->get('user/notifications', 'NotificationsController@index')
                 ->name('api.user.notifications.index');
+            // 未读通知统计
+            $api->get('user/notifications/stats', 'NotificationsController@stats')
+                ->name('api.user.notifications.stats');
         });
     });
 });
